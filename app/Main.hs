8dbf076d963f8d4427642d0a275bc8875f88409d
tsukimizake <shomasd@gmail.com>
2023-11-04 18:20:43 +0900

    ver1

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,57 @@
 module Main where
 
+import Data.Function ((&))
 import OpenSCAD as OS
 import Prelude
-import Data.Function ((&))
-
 
 sample :: OS.Model3d
-sample = OS.cube 11
+sample =
+  adapterHull
+    `difference` adapterInner
+    `difference` adapterWindow
+    `difference` upperLeverWindow
+    `difference` hookReceiver
+    `difference` boltHoles
 
+adapterHull :: OS.Model3d
+adapterHull = OS.box 100 65 14
+
+adapterInner :: OS.Model3d
+adapterInner =
+  OS.box 80 61 4
+    & OS.translate (10, 0, 5)
+    & OS.translate (0, -1, 0)
+
+adapterWindow :: OS.Model3d
+adapterWindow =
+  OS.box 60 61 12
+    & OS.translate (20, -1, -3)
+
+upperLeverWindow :: OS.Model3d
+upperLeverWindow =
+  OS.box 20 61 6
+    & OS.translate (40, 20, -1)
+
+hookReceiver :: OS.Model3d
+hookReceiver =
+  OS.box 20 7 13
+    & OS.translate (40, 43, -1)
+
+-- pitch: 36mm, 15mm 5M bolt
+boltHoles :: OS.Model3d
+boltHoles =
+  OS.union
+    [ OS.cylinder 2.6 30 def,
+      OS.cylinder 2.6 30 def & OS.translate (15, 0, 0),
+      OS.cylinder 2.6 30 def & OS.translate (0, 36, 0),
+      OS.cylinder 2.6 30 def & OS.translate (15, 36, 0)
+    ]
+    & OS.translate (42.5, 4, 0)
+
+boltHeadHole :: OS.Model3d
+boltHeadHole =
+  OS.cylinder 8 10 def
+    & OS.translate (0, 0, -1)
 
 main :: IO ()
 main = do
--- product.scad
+++ product.scad
@@ -1,1 +1,23 @@
-cube([11.0, 11.0, 11.0]);
+difference() {
+  difference() {
+    difference() {
+      difference() {
+        difference() {
+          cube([100.0, 65.0, 14.0]);
+          translate([0.0,-1.0,0.0]) 
+            translate([10.0,0.0,5.0]) cube([80.0, 61.0, 4.0]);
+        }
+        translate([20.0,-1.0,-3.0]) cube([60.0, 61.0, 12.0]);
+      }
+      translate([40.0,20.0,-1.0]) cube([20.0, 61.0, 6.0]);
+    }
+    translate([40.0,43.0,-1.0]) cube([20.0, 7.0, 13.0]);
+  }
+  translate([42.5,4.0,0.0]) 
+    union() {
+      cylinder(r=2.6, h=30.0);
+      translate([15.0,0.0,0.0]) cylinder(r=2.6, h=30.0);
+      translate([0.0,36.0,0.0]) cylinder(r=2.6, h=30.0);
+      translate([15.0,36.0,0.0]) cylinder(r=2.6, h=30.0);
+    }
+}
