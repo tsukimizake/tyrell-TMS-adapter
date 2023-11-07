41fcbc522a1b119fcca814661573d660f4cef22e
tsukimizake <shomasd@gmail.com>
2023-11-07 21:00:33 +0900

    wider boltHole

--- app/Main.hs
+++ app/Main.hs
@@ -6,30 +6,34 @@ import Prelude
 
 sample :: OS.Model3d
 sample =
-  -- adapterInner
+  adapterInner
 
-  OS.diff
-    adapterHull
-    [ adapterInner,
-      adapterWindow,
-      upperLeverWindow,
-      hookReceiver,
-      boltHoles
-    ]
+-- OS.diff
+--   adapterHull
+--   [ adapterInner,
+--     adapterWindow,
+--     upperLeverWindow,
+--     hookReceiver,
+--     boltHoles
+--  ]
 
 adapterHull :: OS.Model3d
 adapterHull = OS.box 100 65 14
 
 adapterInner :: OS.Model3d
 adapterInner =
-  let v0 = (0, 0, 0)
-      v1 = (82, 0, 0)
-      v2 = (79.5, 61, 0)
-      v3 = (2, 61, 0)
-      v4 = (0, 0, 4.4)
-      v5 = (82, 0, 4.4)
-      v6 = (79.5, 61, 3.9)
-      v7 = (2, 61, 3.9)
+  let topL = 1.5 + 0.6
+      topR = 83 + 1.2
+      botL = 0
+      botR = 84 + 1.2
+      v0 = (botL, 0, 0)
+      v1 = (botR, 0, 0)
+      v2 = (topR, 61, 0)
+      v3 = (topL, 61, 0)
+      v4 = (botL, 0, 4.4)
+      v5 = (botR, 0, 4.4)
+      v6 = (topR, 61, 3.9)
+      v7 = (topL, 61, 3.9)
    in OS.unsafePolyhedron
         10
         [v0, v1, v2, v3, v4, v5, v6, v7]
@@ -56,13 +60,13 @@ hookReceiver =
 boltHoles :: OS.Model3d
 boltHoles =
   OS.union
-    [ OS.cylinder 2.6 30 def,
+    [ OS.cylinder 2.7 30 def,
       boltHeadHole,
-      OS.cylinder 2.6 30 def & OS.translate (15, 0, 0),
+      OS.cylinder 2.7 30 def & OS.translate (15, 0, 0),
       boltHeadHole & OS.translate (15, 0, 0),
-      OS.cylinder 2.6 30 def & OS.translate (0, 36, 0),
+      OS.cylinder 2.7 30 def & OS.translate (0, 36, 0),
       boltHeadHole & OS.translate (0, 36, 0),
-      OS.cylinder 2.6 30 def & OS.translate (15, 36, 0),
+      OS.cylinder 2.7 30 def & OS.translate (15, 36, 0),
       boltHeadHole & OS.translate (15, 36, 0)
     ]
     & OS.translate (42.5, 19, 0)
