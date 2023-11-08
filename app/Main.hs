798758974599d2c830b85ccdc862ab2f44726465
tsukimizake <shomasd@gmail.com>
2023-11-08 20:53:03 +0900

    mirror refactor

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,13 @@ import OpenSCAD as OS
 import Prelude
 
 -- botLR = 85.2
--- topLR = 2.1
+-- topLR = 82.1
 -- botZ = 4.73
 -- topZ = 4.23
 -- winW = 61
 -- levWinW = 22
 sample :: OS.Model3d
 sample =
-  -- adapterInner
-
   OS.diff
     adapterHull
     [ adapterInner,
@@ -24,16 +22,24 @@ sample =
     ]
 
 adapterHull :: OS.Model3d
-adapterHull = OS.box 95 63 12 & OS.translate (2.5, 0, 1.5)
+adapterHull =
+  OS.intersection
+    [ OS.minkowski
+        [ OS.box 47 61 9,
+          OS.cylinder 1 1 def
+        ],
+      OS.cube 200 & OS.translate (0, -10, 0)
+    ]
+    & OS.translate (0, 0, 2.5)
 
 adapterInner :: OS.Model3d
 adapterInner =
-  let topL = 1.5 + 0.6
-      topR = 83
+  let topL = 0
+      topR = 41
       botL = 0
-      botR = 84 + 1.2
-      botZ = 4.4 + 0.33
-      topZ = 3.9 + 0.33
+      botR = 42.6
+      botZ = 4.73
+      topZ = 4.23
       v0 = (botL, 0, 0)
       v1 = (botR, 0, 0)
       v2 = (topR, 61, 0)
@@ -46,38 +52,33 @@ adapterInner =
         10
         [v0, v1, v2, v3, v4, v5, v6, v7]
         (Faces [[0, 1, 2, 3], [4, 5, 1, 0], [7, 6, 5, 4], [5, 6, 2, 1], [6, 7, 3, 2], [7, 4, 0, 3]])
-        & OS.translate (8, 0, 5)
-        & OS.translate (0, -1, 0)
+        & OS.translate (0, -1, 5)
 
 adapterWindow :: OS.Model3d
 adapterWindow =
-  OS.box 61 61 12
-    & OS.translate (20.5, -1, -3)
+  OS.box 31 61 12
+    & OS.translate (0, -1, -3)
 
 upperLeverWindow :: OS.Model3d
 upperLeverWindow =
-  OS.box 22 61 7
-    & OS.translate (39, 20, -1)
+  OS.box 11 61 7
+    & OS.translate (0, 20, 0)
 
 hookReceiver :: OS.Model3d
 hookReceiver =
-  OS.box 22 7 13
-    & OS.translate (39, 43, -1)
+  OS.box 11 7 13
+    & OS.translate (0, 43, -1)
 
 -- pitch: 36mm, 15mm 5M bolt
 boltHoles :: OS.Model3d
 boltHoles =
   OS.union
-    [ OS.cylinder 2.7 30 def,
+    [ OS.cylinder 2.8 30 def,
       boltHeadHole,
-      OS.cylinder 2.7 30 def & OS.translate (15, 0, 0),
-      boltHeadHole & OS.translate (15, 0, 0),
-      OS.cylinder 2.7 30 def & OS.translate (0, 36, 0),
-      boltHeadHole & OS.translate (0, 36, 0),
-      OS.cylinder 2.7 30 def & OS.translate (15, 36, 0),
-      boltHeadHole & OS.translate (15, 36, 0)
+      OS.cylinder 2.8 30 def & OS.translate (0, 36, 0),
+      boltHeadHole & OS.translate (0, 36, 0)
     ]
-    & OS.translate (42.5, 19, 0)
+    & OS.translate (7.5, 19, 0)
 
 boltHeadHole :: OS.Model3d
 boltHeadHole =
@@ -86,6 +87,6 @@ boltHeadHole =
 main :: IO ()
 main =
   do
-    sample -- <> (sample & mirror (1, 0, 0))
+    sample <> (sample & mirror (1, 0, 0))
     & OS.render
     & writeFile "product.scad"
